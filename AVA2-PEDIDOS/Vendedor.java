fddeba6514270d82ac14b80e5212d556b5376b17
new <122328306+pedro-newlands@users.noreply.github.com>
2025-12-18 21:30:44 -0300

    Delete AVA2-PEDIDOS directory

--- AVA2-PEDIDOS/Vendedor.java
+++
@@ -1,8 +0,0 @@
-public class Vendedor {
-    private String nameVendedor;
-    private int registrationVendedor;
-    public Vendedor (String nameVendedor, int registrationVendedor){
-        this.nameVendedor = nameVendedor;
-        this.registrationVendedor = registrationVendedor;
-    }
-}
