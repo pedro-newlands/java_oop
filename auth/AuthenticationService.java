d65812b98342076117f97d4888db141ab1daf810
new <122328306+pedro-newlands@users.noreply.github.com>
2026-01-04 23:00:35 -0300

    Update AuthenticationService.java

--- auth/AuthenticationService.java
+++ auth/AuthenticationService.java
@@ -1,11 +1,9 @@
 package auth;
 import model.User;
 
-//implementação da interface, uma vez que aqui será dito as regras do negócio
 public class AuthenticationService implements LoginService{
     private User currentUser;
 
-    //polimorfismo de sobrescrita nos métodos da interface, pois é crucial que a assinatura permaneca a mesma da proposta por LoginService
     @Override
     public Boolean login(String username, String password){
         return true;
@@ -16,10 +14,10 @@ public class AuthenticationService implements LoginService{
         System.out.println("saindo");
     }
 
-
     @Override
     public User getCurrentUser(){
         return currentUser;
     } 
-
+    
 }
+
